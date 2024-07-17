1f04bacd8f3bb6b20ebc4fa2324e120649d81e1a
Enzo <enzodearaujogrellmann@hotmail.com>
2024-07-17 11:57:36 -0300

    Suga agressivamente meu picole

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,56 @@
-const transformProject = () => {
-
-    const container = document.getElementsByClassName('container');
-    const el = document.getElementsByClassName("transformProject");
+const backToMain = (event) => {
     
-    anime({
-        targets: el,
-        translateX: [0, 600],
-        opacity: [1, 0],
-        easing: 'easeInOutSine',
-        duration: 200
-    });
+  event.preventDefault;
+
+  const titleContainer = document.getElementsByClassName('title-container')[0];
+
+  console.log(titleContainer)
 }
+
+const transformProject = (event) => {
+
+  event.preventDefault;
+
+  // container
+  const titleContainer = document.getElementsByClassName('title-container')[0];
+  console.log(titleContainer)
+  // projeto
+  const projectContainer = document.getElementsByClassName('project-container')[0];
+  console.log(projectContainer)
+  // arrow
+  const arrow = document.getElementsByClassName('arrow')[0];
+  console.log(arrow)
+
+  const elements = document.getElementsByClassName('transformProject');
+
+  anime({
+    targets: elements,
+    translateX: [0, 600],
+    opacity: [1, 0],
+    easing: 'easeInOutSine',
+    duration: 400
+  });
+
+  setTimeout(() => {
+    titleContainer.style.display = 'none';
+    projectContainer.classList.remove('hide');
+    arrow.classList.remove("hide");
+    
+  }, 800);
+
+  
+}
+
+const cards = document.querySelectorAll('.card');
+
+window.addEventListener('scroll', parallaxEffect);
+
+function parallaxEffect() {
+  let scroll = window.scrollY;
+
+  cards.forEach(card => {
+    let speed = card.getAttribute('data-speed');
+    card.style.transform = `translateY(${scroll * speed}px)`;
+  });
+}
+
