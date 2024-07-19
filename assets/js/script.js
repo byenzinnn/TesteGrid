9acd52a85957732f53282ffb9a3d09547c58a48d
Enzo <enzodearaujogrellmann@hotmail.com>
2024-07-19 11:57:19 -0300

    Items updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,51 @@ const backToMain = (event) => {
     
   event.preventDefault;
 
+  // container
   const titleContainer = document.getElementsByClassName('title-container')[0];
-
   console.log(titleContainer)
+  // projeto
+  const projectContainer = document.getElementsByClassName('project-container')[0];
+  console.log(projectContainer)
+  // arrow
+  const arrow = document.getElementsByClassName('arrow')[0];
+  console.log(arrow)
+
+  const elements = document.getElementsByClassName('transformProject');
+
+  anime({
+    targets: projectContainer,
+    opacity: [1, 0],
+    translateX: -1000,
+    easing: 'easeInOutSine',
+    duration: 200
+  });
+
+  anime({
+    targets: elements,
+    translateX: [600, 0],
+    opacity: [0, 1],
+    easing: 'easeInOutSine',
+    duration: 300
+  });
+
+  setTimeout(() => {
+    titleContainer.style.display = 'flex';
+    titleContainer.style.opacity = '1';
+    anime({
+      targets: elements,
+      translateX: [600, 0],
+      opacity: [0, 1],
+      easing: 'easeInOutSine',
+      duration: 600
+    });
+
+    
+    projectContainer.classList.add('hide');
+  }, 800);
 }
+
+//////////////////////////////////////////////////////////////////
 
 const transformProject = (event) => {
 
@@ -28,18 +69,24 @@ const transformProject = (event) => {
     translateX: [0, 600],
     opacity: [1, 0],
     easing: 'easeInOutSine',
-    duration: 400
+    duration: 300
   });
 
   setTimeout(() => {
     titleContainer.style.display = 'none';
-    projectContainer.classList.remove('hide');
-    arrow.classList.remove("hide");
     
-  }, 800);
-
-  
+    anime({
+      targets: projectContainer,
+      opacity: [0, 1],
+      translateX: [-600, 0],
+      easing: 'easeInOutSine',
+      duration: 150
+    })
+    projectContainer.classList.remove('hide');
+  }, 600);
 }
+
+///////////////////////////////////////////////
 
 const cards = document.querySelectorAll('.card');
 
